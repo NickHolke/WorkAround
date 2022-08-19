0cd1ad0626601d2618b3d4e409b583976ad52eb7
Nick Holke <nickholke@Nicks-MacBook-Pro-2.local>
2022-08-19 09:53:36 -0700

    Removes second type of paywall as well with mutation observer

--- contentScript.js
+++ contentScript.js
@@ -14,21 +14,31 @@
 // Add a mutation observer on #root to watch for slower loading paywall
 
 function nytimesPaywall() {
-  //   const root = document.querySelector('#app');
-  //   const mutationCallback = mutationList => {
-  //     for (const mutation of mutationList) {
-  //       console.log('MUTATION');
-  //       console.log(mutation);
-  //     }
-  //   };
-  //   const config = { childList: true, subtree: true };
-  //   const observer = new MutationObserver(mutationCallback);
-  //   observer.observe(root, config);
   const paywall = document.querySelector('#gateway-content');
+
   if (paywall) {
-    paywall.style.display = 'none';
-    const overlay = document.querySelector('#app > div > div:first-child');
-    overlay.style.position = 'static';
-    overlay.querySelector(':scope > div:last-child').style.display = 'none';
+    removePaywall(paywall);
+  } else {
+    const root = document.querySelector('#app');
+    const mutationCallback = mutationList => {
+      for (const mutation of mutationList) {
+        mutation.addedNodes.forEach(node => {
+          if (node.id === 'gateway-content') {
+            removePaywall(node);
+            observer.disconnect();
+          }
+        });
+      }
+    };
+    const config = { childList: true, subtree: true };
+    const observer = new MutationObserver(mutationCallback);
+    observer.observe(root, config);
   }
+}
+
+function removePaywall(paywall) {
+  paywall.style.display = 'none';
+  const overlay = document.querySelector('#app > div > div:first-child');
+  overlay.style.position = 'static';
+  overlay.querySelector(':scope > div:last-child').style.display = 'none';
 }
