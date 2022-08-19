7485129162deeab9f31a1751cad1c8095b45b583
Nick Holke <nickholke@Nicks-MacBook-Pro-2.local>
2022-08-19 10:48:22 -0700

    removed extra function

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
-function handleUpdated(tabId, changeInfo, tabInfo) {
-  console.log(`Updated tab: ${tabId}`);
-  console.log('Changed attributes: ', changeInfo);
-  console.log('New tab Info: ', tabInfo);
-}
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
   if (changeInfo.status === 'complete' && tabInfo.url) {
-    if (tabInfo.url.includes('nytimes'))
+    if (tabInfo.url.includes('www.nytimes'))
       chrome.tabs.sendMessage(tabId, {
         site: 'nytimes'
       });
