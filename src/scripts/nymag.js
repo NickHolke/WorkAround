5750c0bf3d32a9e3571ccf937dd97ee9d2c75df2
Nick Holke <nickholke@Nicks-MacBook-Pro-2.local>
2022-08-25 21:48:52 -0700

    add back paragraphs that are removed from article

--- src/scripts/nymag.js
+++ src/scripts/nymag.js
@@ -1,15 +1,26 @@
 import { getNode, removeBasicPaywall, initMutationObserver } from './helpers';
 
 export default function nymag() {
-  const root = getNode('html');
-  const paywall = getNode('#cliff-takeover');
+  // const root = getNode('html');
+  // const paywall = getNode('#cliff-takeover');
+  // if (root.hasAttribute('style') && paywall) {
+  //   removeBasicPaywall(paywall, document.body, root);
+  // } else {
+  //   const config = { attributes: true, attributeFilter: ['style'] };
+  //   initMutationObserver(root, config, nymMutationCallback);
+  // }
+  const articleNode = getNode('.article-content');
+  const config = { childList: true, subtree: true };
+  const observer = new MutationObserver((mutationList, observer) => {
+    for (const mutation of mutationList) {
+      const { removedNodes, nextSibling, target: parentNode } = mutation;
+      if (mutation.removedNodes.length) {
+        parentNode.insertBefore(removedNodes[0], nextSibling);
+      }
+    }
+  });
 
-  if (root.hasAttribute('style') && paywall) {
-    removeBasicPaywall(paywall, document.body, root);
-  } else {
-    const config = { attributes: true, attributeFilter: ['style'] };
-    initMutationObserver(root, config, nymMutationCallback);
-  }
+  observer.observe(document.body, config);
 }
 
 function nymMutationCallback(mutationList, observer) {
